93b1cd47c98f283d3ea2c02a7e14605b7a03774b
Taylor Hudson <taylorhudsonsantos@gmail.com>
2023-02-08 09:29:14 -0300

    Pagination of the news in the api

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js";
+import { createService, findAllService, countNews } from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {
@@ -6,7 +6,7 @@ const create = async (req, res) => {
 
     if (!title || !banner || !text)
       return res.status(400).send({ message: 'Submit all fields to create' });
- 
+
     await createService({
       title,
       banner,
@@ -22,8 +22,43 @@ const create = async (req, res) => {
 
 const findAll = async (req, res) => {
   try {
-    const news = await findAllService();
-    res.send(news);
+    let { limit, offset } = req.query;
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) limit = 5;
+    if (!offset) offset = 0;
+
+    const total = await countNews();
+    const nextOffset = offset + limit;
+    const currentURL = req.baseUrl;
+
+    const nextUrl = nextOffset < total ? `${currentURL}?limit=${limit}&offset=${nextOffset}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous !== null ? `${currentURL}?limit=${limit}&offset=${previous}` : null;
+
+    const news = await findAllService(limit, offset);
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+      result: news.map((i) => ({
+        id: i._id,
+        title: i.title,
+        text: i.text,
+        banner: i.banner,
+        likes: i.likes,
+        comments: i.comments,
+        user: {
+          name: i.user.name,
+          username: i.user.username,
+          avatar: i.user.avatar
+        }
+      }))
+    });
   } catch (err) {
     return res.status(500).send({ message: err.message });
   }
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -1,6 +1,15 @@
 import { News } from '../models/News.js';
 
 const createService = (body) => News.create(body);
-const findAllService = () => News.find();
 
-export { createService, findAllService };
+const findAllService = (limit, offset) => 
+  News
+    .find()
+    .sort({_id: -1}) //Brings items starting from the last one added
+    .skip(offset)
+    .limit(limit)
+    .populate('user'); //Populates the request with all user data
+
+const countNews = () => News.countDocuments();
+
+export { createService, findAllService, countNews }; 
